05ea6437e51bdbcd6e944918d7ac189bfa35df44
Zhaomin <zhaomin805@gmail.com>
2025-08-07 17:19:33 +0800

    Add mask option

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -24,6 +24,26 @@ def convert_pr_units(da):
     
     return da
 
+def apply_mask(da, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+   
+    Args:
+     da (xarray.DataArray): Data to mask
+     sftlf_file (str): Land surface fraction file
+     realm (str): Realm to mask
+   
+    """
+  
+    ds = xr.open_dataset(sftlf_file)
+  
+    if realm == 'land':
+        masked_da = da.where(ds['sftlf'].data < 50)
+    else:
+        masked_da = da.where(ds['sftlf'].data > 50)   
+  
+    return masked_da
+        
+
 def create_plot(clim, model, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
     
@@ -66,6 +86,10 @@ def main(inargs):
     clim = ds["pr"].groupby("time.season").mean("time", keep_attrs=True)
     clim = convert_pr_units(clim)
 
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
+
     create_plot(clim, ds.attrs["source_id"], inargs.season,gridlines=inargs.gridlines,levels=inargs.cbar_levels)
     plt.savefig(
         inargs.output_file,
@@ -84,6 +108,8 @@ if __name__ == "__main__":
     parser.add_argument("output_file", type=str, help="Output file name")
     parser.add_argument("--gridlines",action="store_true",default=False,help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels",type=float,nargs="*",default=None,help="list of levels / tick marks to appear on the colorbar")
+    parser.add_argument("--mask",type=str,nargs=2,metavar=("SFTLF_FILE", "REALM"),default=None,help=
+                        """Provide sftlf file and realm to mask ('land' or 'ocean')""")
 
     args = parser.parse_args()
 
